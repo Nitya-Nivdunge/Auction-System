5250f409696b86fb57f548eb92f0f3a04d60eefc
Nitya Nivdunge <77236109+Nitya-Nivdunge@users.noreply.github.com>
2025-05-15 16:52:58 +0530

    Delete Auction System/db.py

--- Auction System/db.py
+++
@@ -1,8 +0,0 @@
-import mysql.connector
-
-def get_db_connection(): 
-    return mysql.connector.connect( host="localhost", # Update if needed 
-                                   user="root", # Update with your MySQL username 
-                                   password="", # Update with your MySQL password 
-                                   database="auction_db" # Make sure this DB exists 
-                                )
